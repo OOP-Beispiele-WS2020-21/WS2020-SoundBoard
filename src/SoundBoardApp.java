3acd754f465d7697bd4029b8e6eead6db64df823
Alexander Bazo <alexanderbazo@googlemail.com>
2021-01-19 11:16:21 +0100

    Prepare starter code

--- src/SoundBoardApp.java
+++ src/SoundBoardApp.java
@@ -1,14 +1,6 @@
 import config.AppConfig;
 import de.ur.mi.oop.app.GraphicsApp;
-import de.ur.mi.oop.events.MousePressedEvent;
-import de.ur.mi.oop.graphics.Label;
 import de.ur.mi.oop.launcher.GraphicsAppLauncher;
-import sounds.SoundFileNotFoundException;
-import sounds.SoundManager;
-import ui.Button;
-import ui.ButtonClickListener;
-
-import java.util.ArrayList;
 
 /**
  * Einfaches SoundBoard auf Basis der GraphicsApp
@@ -27,94 +19,17 @@ import java.util.ArrayList;
  * Alle verfügbaren Sounds werden über ein Enum repräsentiert.
  */
 
-public class SoundBoardApp extends GraphicsApp implements ButtonClickListener {
+public class SoundBoardApp extends GraphicsApp {
 
-    private ArrayList<Button> buttons;
-    private Label appLabel;
-    private SoundManager soundManager;
 
     @Override
     public void initialize() {
         setCanvasSize(AppConfig.DISPLAY_WIDTH, AppConfig.DISPLAY_HEIGHT);
-        soundManager = new SoundManager();
-        initButtons();
-        initAppLabel();
-    }
-
-    private void initButtons() {
-        // Bereich, der im UI für einen Button zur Verfügung steht
-        int buttonBoxWidth = AppConfig.BUTTON_WIDTH + (2 * AppConfig.BUTTON_MARGIN);
-        buttons = new ArrayList<>();
-        for (int x = 0; x < AppConfig.GRID_SIZE; x++) {
-            for (int y = 0; y < AppConfig.GRID_SIZE; y++) {
-                /*
-                 Berechnung der ID auf Basis der aktuellen Zählervariablen der beiden Schleifen. Der numerische Wert wird
-                 dabei durch die valueOf-Methode in eine String umgewandelt. Für die gleichmäßige Darstellung wird eine
-                 führende Null ergänzt, wenn die ID kleiner als 10 ist.
-                 */
-                String buttonID = String.valueOf(y * AppConfig.GRID_SIZE + x + 1);
-                if (buttonID.length() < 2) {
-                    buttonID = "0" + buttonID;
-                }
-                Button button = new Button(x * buttonBoxWidth, y * buttonBoxWidth);
-                button.setText(buttonID);
-                button.setListener(this);
-                buttons.add(button);
-            }
-        }
-    }
-
-    private void initAppLabel() {
-        appLabel = new Label(AppConfig.APP_LABEL_POSITION_X, AppConfig.APP_LABEL_POSITION_Y, AppConfig.APP_TITLE);
-        appLabel.setColor(AppConfig.APP_LABEL_COLOR);
-        appLabel.setFontSize(AppConfig.APP_LABEL_FONT_SIZE);
-        appLabel.setFont(AppConfig.DEFAULT_FONT);
-    }
-
-    private void playSound(String name) {
-        // Die playSound-Methode löst eine Exception aus, wenn kein Sound für den übergebenen Parameter gefunden wurde ...
-        try {
-            soundManager.playSound(name);
-            // ... die im Zweifelsfall hier abgefangen werden kann.
-        } catch (SoundFileNotFoundException e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
     public void draw() {
         drawBackground(AppConfig.BACKGROUND_COLOR);
-        drawButtons();
-        appLabel.draw();
-    }
-
-    private void drawButtons() {
-        for (Button button : buttons) {
-            button.draw();
-        }
-    }
-
-    /**
-     * Wird aufgerufen, wenn ein Button zurückmeldet, dass er angeklickt wurde
-     *
-     * @param button Der angeklickte Button
-     */
-    @Override
-    public void onButtonClicked(Button button) {
-        /*
-         Nach dem Klick wird der Button in den Highlight-Zustand versetzt, der nach einer bestimmten Anzahl
-         an Frames automatisch deaktivert wird (siehe Button-Klasse).
-         */
-        button.highlight();
-        // Anhand der Button-Beschriftung wird versucht, den zugehörigen Sound abzuspielen
-        playSound(button.getLabel());
-    }
-
-    @Override
-    public void onMousePressed(MousePressedEvent event) {
-        for (Button button : buttons) {
-            button.handleMouseClick(event.getXPos(), event.getYPos());
-        }
     }
 
     public static void main(String[] args) {
--- src/sounds/SoundFileNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package sounds;
-
-/**
- * Wird ausgelöst, wenn im SoundManager auf eine nicht bekannte SoundDatei zugegriffen wird
- */
-public class SoundFileNotFoundException extends Exception {
-}
