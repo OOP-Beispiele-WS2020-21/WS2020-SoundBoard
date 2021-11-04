8a2c571481fa659ae73630e9302396f63f6dd8a1
Alexander Bazo <alexanderbazo@googlemail.com>
2021-11-04 16:23:59 +0100

    Update GraphicsApp library

--- src/config/AppConfig.java
+++ src/config/AppConfig.java
@@ -9,9 +9,9 @@ import de.ur.mi.oop.colors.Color;
 public abstract class AppConfig {
 
     // Display
-    public static final int DISPLAY_WIDTH = 600;
+    public static final int DISPLAY_WIDTH = 620;
     public static final int DISPLAY_HEIGHT = 700;
-    public static final Color BACKGROUND_COLOR = new Color(226, 232, 206);
+    public static final Color BACKGROUND_COLOR = OOPColors.CREAM;
     public static final int GRID_SIZE = 6;
     // Fonts
     public static final String DEFAULT_FONT = "Arial Rounded MT Bold";
@@ -19,18 +19,18 @@ public abstract class AppConfig {
     public static final String APP_TITLE = "OOP SOUNDBOARD";
     public static final int APP_LABEL_POSITION_X = 25;
     public static final int APP_LABEL_POSITION_Y = 675;
-    public static final int APP_LABEL_FONT_SIZE = 48;
-    public static final Color APP_LABEL_COLOR = new Color(38, 38, 38);
+    public static final int APP_LABEL_FONT_SIZE = 32;
+    public static final Color APP_LABEL_COLOR = OOPColors.GREY;
     // Buttons
     public static final int BUTTON_WIDTH = 50;
     public static final int BUTTON_MARGIN = 25;
     public static final int BUTTON_SHADOW_OFFSET = 5;
-    public static final Color BUTTON_COLOR = new Color(245, 0, 0);
-    public static final Color BUTTON_SHADOW_COLOR = new Color(184, 0, 0);
-    public static final Color BUTTON_HIGHLIGHT_COLOR = new Color(255, 51, 51);
-    public static final int BUTTON_TEXT_MARGIN_RIGHT = 10;
-    public static final int BUTTON_FONT_SIZE = 14;
-    public static final Color BUTTON_FONT_COLOR = new Color(38, 38, 38);
+    public static final Color BUTTON_COLOR = OOPColors.RED;
+    public static final Color BUTTON_SHADOW_COLOR = new Color(150, 49, 63);
+    public static final Color BUTTON_HIGHLIGHT_COLOR = OOPColors.GREEN;
+    public static final int BUTTON_TEXT_MARGIN_RIGHT = 5;
+    public static final int BUTTON_FONT_SIZE = 12;
+    public static final Color BUTTON_FONT_COLOR = OOPColors.GREY;
     public static final int BUTTON_HIGHLIGHT_FRAME_COUNT = 30;
 
 }
